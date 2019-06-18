648b5a4462755595cf451d3da9c38de67d88a0df
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-18 16:38:20 -0700

    error or log

--- logger.js
+++ logger.js
@@ -6,9 +6,16 @@ const client = new net.Socket();
 
 client.connect(3001, 'localhost', () => console.log('Socket in logger.js created!'));
 
+
 client.on('data', data => {
   let payload = JSON.parse(data.toString().trim());
-  console.log(payload);
+  if(payload.name === 'error'){
+    console.error(payload);
+  }else if(payload.name === 'saved'){
+    console.log(payload);
+  }else{
+    console.log('neither error or saved');
+  }
 });
 
 client.on('close', () => {
