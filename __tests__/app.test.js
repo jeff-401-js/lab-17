b11ee55b0e227ea543b8cfcaf34ecd7ec44c10b7
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-19 19:06:08 -0700

    more testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,9 +12,31 @@ describe('uppercase', () => {
 });
 
 describe('logger function', () => {
-  it('should take in a message and console.log it', () => {
+  it('should take in a message and console.log it if a save', () => {
     let test = {
       name: 'saved',
+      data: `SAVED: event just happened!`,
+    };
+    let spy = jest.spyOn(console, 'log');
+    logger.logger(JSON.stringify(test));
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+  });
+
+  it('should take in a message and console.error it if an error', () => {
+    let test = {
+      name: 'error',
+      data: `SAVED: event just happened!`,
+    };
+    let spy = jest.spyOn(console, 'error');
+    logger.logger(JSON.stringify(test));
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+  });
+
+  it('should take in a message and console.log if neither error or save', () => {
+    let test = {
+      name: 'test',
       data: `SAVED: event just happened!`,
     };
     let spy = jest.spyOn(console, 'log');
