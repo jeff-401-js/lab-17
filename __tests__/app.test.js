7333a70be5f670530e190f55360b8d4912338ffb
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-19 19:03:52 -0700

    testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,25 @@
 'use strict';
 
-const app = require('../app.js');
-// console.log(app);
-// const server = require('../server.js');
+const app = require('../utilsApp.js');
+const logger = require('../utilsLogger.js');
 
 describe('uppercase', () => {
   it('should return a string uppercased', () => {
     let str = 'some test string';
-    // console.log(app.upper);
     str = app.upper(str);
     expect(str).toEqual('SOME TEST STRING');
+  });
+});
+
+describe('logger function', () => {
+  it('should take in a message and console.log it', () => {
+    let test = {
+      name: 'saved',
+      data: `SAVED: event just happened!`,
+    };
+    let spy = jest.spyOn(console, 'log');
+    logger.logger(JSON.stringify(test));
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
   });
 });
